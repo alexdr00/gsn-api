247f71062d9f9509a44a56376091c99a0bdeae0a
alexdr00 <alexrendon2109@gmail.com>
2020-06-10 23:19:08 -0500

    Add a better configuration for project compiling
    - Add the first test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   errorOnDeprecated: true,
+  preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleDirectories: ['node_modules', 'src'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.dev.json',
+    },
+  },
 };
